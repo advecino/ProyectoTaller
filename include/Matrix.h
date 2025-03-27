9a9200b003d7ed375645f39e85f9541847a43b06
advecino <advecino@unirioja.es>
2025-03-27 15:23:17 +0100

    comentado

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,31 +1,95 @@
-//
-// Created by advecino on 20/03/2025.
-//
+/**
+ * @file Matrix.h
+ * @brief Declaración de la clase Matrix para operaciones matriciales.
+ * @author advecino
+ * @date 20/03/2025
+ */
 
 #ifndef PROYECTOTALLER_MATRIX_H
 #define PROYECTOTALLER_MATRIX_H
-//
+
+/**
+ * @class Matrix
+ * @brief Clase para representar y manipular matrices.
+ */
 
 class Matrix {
 public:
+    /**
+    * @brief Constructor de la matriz con dimensiones especificadas.
+    * @param fil Número de filas.
+    * @param col Número de columnas.
+    */
     Matrix(int fil, int col);
+
+    /**
+     * @brief Constructor que inicializa la matriz con valores dados.
+     * @param fil Número de filas.
+     * @param col Número de columnas.
+     * @param v Array de valores para inicializar la matriz.
+     * @param n Número de elementos en el array.
+     */
     Matrix(int fil, int col, double v[],int n);
+
+    /**
+    * @brief Constructor de copia.
+    * @param m Objeto Matrix a copiar.
+    */
     Matrix(const Matrix& m);
+
+    /**
+    * @brief Destructor de la matriz.
+    */
     ~Matrix();
 
+
+    /**
+    * @brief Sobrecarga del operador de asignación.
+    * @param matrix2 Matriz a asignar.
+    * @return Referencia a la matriz resultante.
+    */
     Matrix& operator= (const Matrix& matrix2);
+    /**
+    * @brief Sobrecarga del operador de suma.
+    * @param matrix2 Matriz a sumar.
+    * @return Matriz resultante de la suma.
+    */
     Matrix operator+ (const Matrix& matrix2);
+    /**
+     * @brief Sobrecarga del operador de resta.
+     * @param matrix2 Matriz a restar.
+     * @return Matriz resultante de la resta.
+     */
     Matrix operator- (const Matrix& matrix2);
+    /**
+    * @brief Sobrecarga del operador de multiplicación.
+    * @param matrix2 Matriz a multiplicar.
+    * @return Matriz resultante de la multiplicación.
+    */
     Matrix operator* (const Matrix& matrix2);
+    /**
+     * @brief Sobrecarga del operador de acceso a elementos de la matriz.
+     * @param i Índice de la fila.
+     * @param j Índice de la columna.
+     * @return Referencia al valor en la posición (i, j).
+     */
     double& operator()(const int i, const int j) const;
 
+    /**
+    * @brief Imprime la matriz en la consola.
+    */
     void print();
 
 private:
+    /**
+     * @brief Inicializa la matriz con valores en cero.
+     */
     void initMatrix();
+
 private:
-    int fil,col,n;
-    double** matrix;
+    int fil;  ///< Número de filas de la matriz.
+    int col; ///< Número de columnas de la matriz.
+    double** matrix; ///< Puntero a la memoria donde se almacenan los valores de la matriz.
 
 };
 
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,74 +1,168 @@
-//
-// Created by advecino on 20/03/2025.
-//
-//
-
+/**
+ * @file Matrix.cpp
+ * @brief Implementación de la clase Matrix para operaciones matriciales.
+ * @author advecino
+ * @date 20/03/2025
+ */
 #include "../include/Matrix.h"
 #include <iostream>
 #include <iomanip>
 
-Matrix::Matrix(int fil, int col): fil(fil),col(col){
+/**
+ * @brief Constructor de la matriz con dimensiones especificadas.
+ * @param fil Número de filas.
+ * @param col Número de columnas.
+ */
+Matrix::Matrix(int fil, int col) : fil(fil), col(col)
+{
     initMatrix();
 }
 
-
-
-
-void Matrix::initMatrix(){
-    matrix = new double*[fil];
-    for(int i=0;i<fil;i++){
-        matrix[i] = new double[col];
-    }
-    for (int i=0; i<fil;i++){
-        for(int j = 0; j<col;j++){
-            matrix[i][j]= 0.0;
-        }
-    }
-}
-
-Matrix::Matrix(int fil, int col, double *v, int n): fil(fil),col(col) {
+/**
+ * @brief Constructor que inicializa la matriz con valores dados.
+ * @param fil Número de filas.
+ * @param col Número de columnas.
+ * @param v Array de valores para inicializar la matriz.
+ * @param n Número de elementos en el array.
+ */
+Matrix::Matrix(int fil, int col, double v[], int n): fil(fil), col(col)
+{
     initMatrix();
+
     int k = 0;
-    for(int i =0;i<fil;i++){
-        for(int j=0;j<col;j++){
-            if(k<n){
+
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++){
+            if (k < n)
                 matrix[i][j] = v[k++];
-            }else{
+            else
                 matrix[i][j] = 0;
+        }
+}
+
+/**
+ * @brief Constructor de copia.
+ * @param m Objeto Matrix a copiar.
+ */
+Matrix::Matrix(const Matrix& m)
+{
+    *this = m;
+}
+
+
+/**
+ * @brief Inicializa la matriz con valores en cero.
+ */
+void Matrix::initMatrix()
+{
+    matrix = new double*[fil];
+    for (int i = 0; i < fil; i++)
+        matrix[i] = new double[col];
+
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            matrix[i][j] = 0.0;
+}
+
+/**
+ * @brief Destructor de la matriz.
+ */
+Matrix::~Matrix()
+{
+    for (int i = 0; i < fil; i++)
+        delete[] matrix[i];
+
+    delete[] matrix;
+}
+
+/**
+ * @brief Sobrecarga del operador de asignación.
+ * @param matrix2 Matriz a asignar.
+ * @return Referencia a la matriz resultante.
+ */
+Matrix& Matrix::operator=(const Matrix& matrix2)
+{
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            this->matrix[i][j] = matrix2.matrix[i][j];
+
+    return *this;
+}
+
+/**
+ * @brief Sobrecarga del operador de suma.
+ * @param matrix2 Matriz a sumar.
+ * @return Matriz resultante de la suma.
+ */
+Matrix Matrix::operator+(const Matrix& matrix2)
+{
+    Matrix result(fil, col);
+
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            result.matrix[i][j] = matrix[i][j] + matrix2.matrix[i][j];
+
+    return result;
+}
+
+/**
+ * @brief Sobrecarga del operador de resta.
+ * @param matrix2 Matriz a restar.
+ * @return Matriz resultante de la resta.
+ */
+Matrix Matrix::operator-(const Matrix& matrix2)
+{
+    Matrix result(fil, col);
+
+    for (int i = 0; i < fil; i++)
+        for (int j = 0; j < col; j++)
+            result.matrix[i][j] = matrix[i][j] - matrix2.matrix[i][j];
+
+    return result;
+}
+
+/**
+ * @brief Sobrecarga del operador de multiplicación.
+ * @param matrix2 Matriz a multiplicar.
+ * @return Matriz resultante de la multiplicación.
+ */
+Matrix Matrix::operator*(const Matrix& matrix2)
+{
+    Matrix result(fil, col);
+
+    for (int i = 0; i < this->fil ; i++){
+        for (int j = 0; j < matrix2.col; j++){
+            result.matrix[i][j] = 0;
+            for (int k = 0; k < this->col; k++){
+                result.matrix[i][j] = result.matrix[i][j] + this->matrix[i][k] * matrix2.matrix[k][j];
             }
         }
     }
 
+    return result;
 }
 
-Matrix::Matrix(const Matrix &m) {
-
+/**
+ * @brief Sobrecarga del operador de acceso a elementos de la matriz.
+ * @param i Índice de la fila.
+ * @param j Índice de la columna.
+ * @return Referencia al valor en la posición (i, j).
+ */
+double& Matrix::operator()(const int i, const int j) const
+{
+    return matrix[i-1][j-1];
 }
 
-Matrix::~Matrix() {
-
-}
-
-Matrix &Matrix::operator=(const Matrix &matrix2) {
-    return <#initializer#>;
-}
-
-Matrix Matrix::operator+(const Matrix &matrix2) {
-    return Matrix(0, 0);
-}
-
-Matrix Matrix::operator-(const Matrix &matrix2) {
-    return Matrix(0, 0);
-}
-
-Matrix Matrix::operator*(const Matrix &matrix2) {
-    return Matrix(0, 0);
-}
-
-double &Matrix::operator()(const int i, const int j) const {
-    return <#initializer#>;
-}
-
-void Matrix::print() {
-
+/**
+ * @brief Imprime la matriz en la consola.
+ */
+void Matrix::print()
+{
+    for (int i = 0; i < fil; i++){
+        for (int j = 0; j < col; j++){
+            std::cout << std::fixed << std::setprecision(14) << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
 }
