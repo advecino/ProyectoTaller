a36b51d46ee761903a68119b57a074182b0568f7
advecino <advecino@unirioja.es>
2025-05-14 18:53:26 +0200

    Continuacion

---
+++ src/IERS.cpp
@@ -0,0 +1,56 @@
+//
+// Created by adria on 11/05/2025.
+//asta squi
+
+#include <stdexcept>
+#include "../include/IERS.h"
+#include "../include/global.h"
+#include "../include/Sat_const.h"
+#include <cmath>
+
+
+/*
+%--------------------------------------------------------------------------
+%
+% IERS: Management of IERS time and polar motion data
+%
+% Last modified:   2018/02/01   M. Mahooti
+%
+%--------------------------------------------------------------------------*/
+
+
+
+IERSResult IERS(Matrix& eop, double Mjd_UTC, char interp) {
+    IERSResult res{};
+    if (interp != 'l' && interp != 'n') interp = 'n';
+    double mjd_floor = std::floor(Mjd_UTC);
+    int cols = eop.getColumnas();
+    int idx = -1;
+    for (int j = 1; j <= cols; ++j) {
+        if (std::floor(eop(4, j)) == mjd_floor) { idx = j; break; }
+    }
+    if (idx < 1 || idx >= cols)
+        throw std::out_of_range("Mjd_UTC fuera de rango en eop");
+    Matrix pre = eop.getSubMatrix(1,13,idx,idx);
+    Matrix nxt(0,0);
+    double fixf = 0.0;
+    if (interp == 'l') {
+        nxt = eop.getSubMatrix(1,13,idx+1,idx+1);
+        fixf = Mjd_UTC - mjd_floor;
+    }
+    auto lerp = [&](int row) {
+        double v0 = pre(row,1);
+        return (interp=='l') ? v0 + (nxt(row,1)-v0)*fixf : v0;
+    };
+    res.x_pole  = lerp(5)/Arcs;
+    res.y_pole  = lerp(6)/Arcs;
+    res.UT1_UTC = lerp(7);
+    res.LOD     = lerp(8);
+    res.dpsi    = lerp(9)/Arcs;
+    res.deps    = lerp(10)/Arcs;
+    res.dx_pole = lerp(11)/Arcs;
+    res.dy_pole = lerp(12)/Arcs;
+    res.TAI_UTC = lerp(13);
+    return res;
+}
+
